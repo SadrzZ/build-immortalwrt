b74ab6d00ebe2959e771b1da1a7c3add07aef416
SadrzZ <132812428+SadrzZ@users.noreply.github.com>
2023-07-25 20:13:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,7 +3,7 @@
 #sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # update feed
-rm -rf feeds/packages/net/xray-core && svn co https://github.com/immortalwrt/packages/branches/master/net/xray-core feeds/packages/net/xray-core
-rm -rf feeds/packages/lang/golang && svn co https://github.com/immortalwrt/packages/branches/master/lang/golang feeds/packages/lang/golang
-rm -rf feeds/luci/applications/luci-app-openclash && svn co https://github.com/immortalwrt/luci/branches/master/applications/luci-app-openclash feeds/luci/applications/luci-app-openclash
+rm -rf feeds/packages/net/xray-core && svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/net/xray-core feeds/packages/net/xray-core
+rm -rf feeds/packages/lang/golang && svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/lang/golang feeds/packages/lang/golang
+rm -rf feeds/luci/applications/luci-app-openclash && svn co https://github.com/immortalwrt/luci/branches/openwrt-23.05/applications/luci-app-openclash feeds/luci/applications/luci-app-openclash
 
